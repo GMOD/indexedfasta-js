1dec9dba15daa5b43ec4e036441a0fd81b4cc3b5
Colin <colin.diesh@gmail.com>
2018-08-20 16:42:13 -0400

    Use fasta parser from cram-js test lib

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-import {
-  parseStream,
-  parseFile,
-  parseStringSync,
-  formatSync,
-  formatStream,
-  formatFile,
-} from './api'
+const IndexedFasta = require('./indexedFasta')
 
-import * as util from './util'
-
-export default {
-  parseStream,
-  parseFile,
-  parseStringSync,
-  formatSync,
-  formatStream,
-  formatFile,
-  util,
+function parseSmallFasta(text) {
+  return text
+    .split('>')
+    .filter(t => /\S/.test(t))
+    .map(entryText => {
+      let [defLine, ...seqLines] = entryText.split('\n')
+      let [id, ...description] = defLine.split(' ')
+      description = description.join(' ')
+      seqLines = seqLines.join('')
+      const sequence = seqLines.replace(/\s/g, '')
+      return { id, description, sequence }
+    })
 }
+
+class FetchableSmallFasta {
+  constructor(filehandle) {
+    this.data = filehandle.readFile().then(buffer => {
+      const text = buffer.toString('utf8')
+      return parseSmallFasta(text)
+    })
+  }
+
+  async fetch(id, start, end) {
+    const data = await this.data
+    const entry = data[id]
+    const length = end - start + 1
+    if (!entry) throw new Error(`no sequence with id ${id} exists`)
+    return entry.sequence.substr(start - 1, length)
+  }
+
+  async getSequenceList() {
+    const data = await this.data
+    return data.map(entry => entry.id)
+  }
+}
+
+module.exports = { parseSmallFasta, FetchableSmallFasta, IndexedFasta }
