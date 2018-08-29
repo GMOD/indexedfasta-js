f3654045934372a5453c94fcadf8f0a0c379c7da
Colin <colin.diesh@gmail.com>
2018-08-29 17:25:17 -0400

    Allow paths for bgzip fasta

--- src/bgzipIndexedFasta.js
+++ src/bgzipIndexedFasta.js
@@ -2,12 +2,13 @@ import { BgzfFilehandle } from '@gmod/bgzf-filehandle'
 import IndexedFasta from './indexedFasta'
 
 class BgzipIndexedFasta extends IndexedFasta {
-  constructor({ fasta, fai, gzi, chunkSizeLimit = 50000 }) {
-    super({ fasta, fai, chunkSizeLimit })
-    this.fasta = new BgzfFilehandle({ filehandle: fasta, gziFilehandle: gzi })
-    this.fai = fai
-
-    this.chunkSizeLimit = chunkSizeLimit
+  constructor({ fasta, path, fai, faiPath, gzi, gziPath, chunkSizeLimit = 50000 }) {
+    super({ fasta, path, fai, faiPath, chunkSizeLimit })
+    if(fasta && gzi) {
+      this.fasta = new BgzfFilehandle({ filehandle: fasta, gziFilehandle: gzi })
+    } else if(path && gziPath) {
+      this.fasta = new BgzfFilehandle({ path, gziPath })
+    }
   }
 }
 
