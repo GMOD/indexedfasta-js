b3bbbd031247dd0d475b96e831581e7828b6235a
Colin <colin.diesh@gmail.com>
2018-08-31 18:50:58 -0400

    Yarn

--- src/bgzipIndexedFasta.js
+++ src/bgzipIndexedFasta.js
@@ -2,15 +2,7 @@ import { BgzfFilehandle } from '@gmod/bgzf-filehandle'
 import IndexedFasta from './indexedFasta'
 
 class BgzipIndexedFasta extends IndexedFasta {
-  constructor({
-    fasta,
-    path,
-    fai,
-    faiPath,
-    gzi,
-    gziPath,
-    chunkSizeLimit,
-  }) {
+  constructor({ fasta, path, fai, faiPath, gzi, gziPath, chunkSizeLimit }) {
     super({ fasta, path, fai, faiPath, chunkSizeLimit })
     if (fasta && gzi) {
       this.fasta = new BgzfFilehandle({ filehandle: fasta, gziFilehandle: gzi })
